4d22310f42ed66dd5e328d63098307bd5ea0c08c
DuyNguyen <nguyenducduy160903@gmail.com>
2023-12-21 23:22:28 +0700

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:chat_app_flutter/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.web,
+  );
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Text('Hello World!'),
+        ),
+      ),
+    );
+  }
+}
